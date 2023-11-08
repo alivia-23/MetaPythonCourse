52a01ecdec80f84daf8ee9cfc6f2ce8363cd4e97
alivia-23 <guin.al@husky.neu.edu>
2023-11-08 00:29:37 -0800

    string opeartions in python

--- python_syntax.py
+++ python_syntax.py
@@ -4,30 +4,50 @@
 # print(b)
 # print(c)
 
-a = 10
+# a = 10
+# # print(a)
+# # del a
+# # print(a)
+# b = "hello"
+
+# print(type(a))
+# b = "world"
+# print(type(b))
+# print(b)
+
+# a = "This is a single line string"
 # print(a)
-# del a
-# print(a)
-b = "hello"
+# b = "This is a multi line" \
+#     "string. It'll show in a single line "
+# print(b)
 
-print(type(a))
-b = "world"
-print(type(b))
-print(b)
+# #Concatenatiom
+# a = "hello"
+# b = "world"
 
-a = "This is a single line string"
-print(a)
-b = "This is a multi line" \
-    "string. It'll show in a single line "
-print(b)
+# c = a + b
+# print(c)
 
-#Concatenatiom
-a = "hello"
-b = "world"
+# name = 'John'
+# print(name[0]) # print the first character of name
+# print(len(name)) # print the size of the variable name
 
-c = a + b
-print(c)
+# print("Where do you live?")
+# location = input()
+# print("So you live in " + location)
 
-name = 'John'
-print(name[0]) # print the first character of name
-print(len(name)) # print the size of the variable name
+# def say_hello():
+#     return "Hello there!"
+
+# # With parameters
+# def say_hello(you):
+#     return "Hello " +  you
+
+# sep is used as separator
+print("Hello", "world", sep=' , ')
+
+# input function
+str1 = input("Please enter a first name: ")
+str2 = input("Please enter a second name: ")
+
+print("hello {} {}".format(str1, str2))
