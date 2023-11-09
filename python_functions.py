322f2d7ebbb438e8857513aad6d6a01e3ecb4a5a
alivia-23 <guin.al@husky.neu.edu>
2023-11-09 15:32:23 -0800

    function scoping

--- python_functions.py
+++ python_functions.py
@@ -4,3 +4,16 @@ def calculate_tax(bill, tax_rate):
 print('Total Tax: ',calculate_tax(175.00, 15))
 
 print('Total Tax: ', calculate_tax(164.33, 22))
+
+#Global function and function scoping
+my_global = 10
+
+def fn1():
+    enclosed_v = 5
+
+    def fn2():
+        local_v = 5
+        print('Access to global', my_global)
+        print("Access to enclosed", enclosed_v)
+    fn2()
+fn1()
