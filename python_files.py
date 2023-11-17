c3536e0461396fccf82013e630a1abc5912aefc8
alivia-23 <guin.al@husky.neu.edu>
2023-11-17 15:55:50 -0800

    write mode in python

--- python_files.py
+++ python_files.py
@@ -1,2 +1,6 @@
 with open('newfile.txt', 'w') as file:
-    file.write('This a new file created!')
+    #file.write('This a new file created!')
+    file.writelines(['This a new file created!', '\nThis is another line to be added'])
+
+# write multiple lines in a file using writelines() function 
+# a writelines() functions accepts a list
