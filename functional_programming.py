d8a221e726085ef9087023f16a2acabeee73f3fc
alivia-23 <guin.al@husky.neu.edu>
2023-11-21 23:25:29 -0800

    functional programming in python

---
+++ functional_programming.py
@@ -0,0 +1,6 @@
+coffees = ['Espresso', 'Latte', 'Cappucino', 'Macchiato', 'Americano', 'Decaf']
+def reverse(str):
+    return str[::-1]
+reversed_coffees = map(reverse, coffees)
+for x in reversed_coffees:
+    print(x)
