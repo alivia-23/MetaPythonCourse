04808825fbcb58d99cd88635c1f6466b73492c0a
alivia-23 <guin.al@husky.neu.edu>
2024-04-01 23:28:19 -0700

    string reversal by recursion

--- string_reversal2.py
+++ string_reversal2.py
@@ -3,7 +3,9 @@ def string_reverse(str):
     if len(str) == 0:
         return str
     else:
-        return string_reverse(str[1:]) + str[0]
+        first = str[0]
+        rem = str[1:]
+        return string_reverse(rem) + first
     
 str = "reversal"
 reverse = string_reverse(str)
