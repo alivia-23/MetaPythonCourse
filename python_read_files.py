d8483f4306e813bdcc634ce6713de447ce62bb42
alivia-23 <guin.al@husky.neu.edu>
2024-04-08 14:49:59 -0700

    delete redundant files

--- python_read_files.py
+++
@@ -1,9 +0,0 @@
-with open('sample.txt', 'r') as file:
-    #print(file.read()) # prints entire contents of the file
-    #print(file.read(44)) # prints first 44 characters of the file
-    #print(file.readline()) # prints only the very first line
-    #print(file.readlines()) # prints entire contents in square brackets
-
-
-    for x in file:
-        print(x)
