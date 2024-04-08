5439a58c46619bbc1a0c9ac17e518636bb8c8927
alivia-23 <guin.al@husky.neu.edu>
2024-04-08 14:46:40 -0700

    delete redundant files

--- python_file_handling.py
+++
@@ -1,9 +0,0 @@
-# file = open('test.txt', mode = 'r')
-# data = file.readline()
-# print(data)
-# file.close()
-
-# Alternate method to access file and print
-with open('test.txt', mode = 'r') as file:
-    data = file.readline()
-print(data)
