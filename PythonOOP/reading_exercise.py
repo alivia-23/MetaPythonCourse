0b07d4ae4a38f98e4e893a4d3341157874e98f7e
alivia-23 <guin.al@husky.neu.edu>
2024-04-10 14:08:24 -0700

    add comments

--- PythonOOP/reading_exercise.py
+++ PythonOOP/reading_exercise.py
@@ -1,13 +1,19 @@
+# Define class MyFirstClass
 class MyFirstClass:
     print("Who wrote this?")
 
+    # Define string variable called index
     index = "Author-Book"
-
+    
+    # Define function hand_list()
     def hand_list(self, philosopher, book):
         self.philosopher = philosopher
         self.book = book
+
+        # variable + “ wrote the book: ” + variable
         print(self.philosopher + " wrote the book: " + self.book)
 
+# Call function handlist()
 whodunnit = MyFirstClass()
 whodunnit.hand_list("Plato", "Republic")
 whodunnit.hand_list("Sun Tzu", "The Art of War")
