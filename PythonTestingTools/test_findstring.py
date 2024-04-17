ee52b787d1b5c3a48c951854c9712c4efa865d9e
alivia-23 <guin.al@husky.neu.edu>
2024-04-16 17:21:24 -0700

    tdd example

---
+++ PythonTestingTools/test_findstring.py
@@ -0,0 +1,9 @@
+from curses.ascii import isdigit
+import pytest
+import findstring
+
+def test_ispresent():
+    assert findstring.ispresent("Al")
+
+def test_nodigit():
+    assert findstring.nodigit("N7")
